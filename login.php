8b890a7add8fd2ecc42a6fe7cf0c03678512df33
Rick Rietbergen <rbps3.2016@gmail.com>
2022-10-18 18:35:05 +0200

    startup login page, url hide

---
+++ login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login.css">
+    <!-- <audio src="https://www.youtube.com/watch?v=Q1oXMy9VZQA" loop></audio> -->
+    <title>Login Overwatch</title>
+</head>
+<body>
+    <main class="m_login">
+        <div class="left"></div>
+
+        <div class="mid">
+            <div class="login_img">
+                <img src="assets/Overwatch_Logo_text.png" alt="logo_text" class="img">
+            </div>
+            <div class="inputs">
+                <label class="login_lab" for="">Account Name</label>
+                <input class="login_inp" type="text">
+                <label class="login_lab" for="">Password</label>
+                <input class="login_inp" type="text">
+
+                <button class="" type="submit">Login</button>
+            </div>
+        </div>
+
+        <div class="right"></div>
+    </main>
+</body>
+</html>
